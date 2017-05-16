096003a9964c734d3986e8365e8ca5a99f3b83be
Stefan Ilie <ilie.stefan92@yahoo.com>
2017-05-16 14:19:10 +0300

    finished data import into db, almost done with interval repeat calling.

--- scrapper.py
+++ scrapper.py
@@ -1,6 +1,8 @@
 import json
 import praw
 import config
+import time
+import threading
 from pymongo import MongoClient
 
 def load_credentials():
@@ -12,30 +14,87 @@ def load_credentials():
         config.PASSWORD = d["password"]
         config.USER_AGENT = d["user_agent"]
 
-def make_call():
+    reddit = praw.Reddit(client_id = config.CLIENT_ID,
+                        client_secret = config.CLIENT_SECRET,
+                        user_agent = config.USER_AGENT,
+                        username = config.USERNAME,
+                        password = config.PASSWORD)
+    return reddit
+
+
+def make_call(reddit, db_conn, subreddit):
     try:
-        reddit = praw.Reddit(client_id = config.CLIENT_ID,
-                            client_secret = config.CLIENT_SECRET,
-                            user_agent = config.USER_AGENT,
-                            username = config.USERNAME,
-                            password = config.PASSWORD)
         #First, get the submisions for the last 2 hours (of today... for now.)
-        submisions = reddit.subreddit("python").submissions(1494844431, 1494851631)
-        for sub in submisions:
-            print sub.title
+        arr_submissions = reddit.subreddit(subreddit).submissions(1494844431, 1494851631)
+
+        for sub in arr_submissions:
+            print "\n\nTitle: ", sub.title
+            submission = {
+                'id': sub.id,
+                'title': sub.title,
+                'selftext': sub.selftext,
+                'num_comments': sub.num_comments,
+                'created': time.time(),
+                'subreddit': subreddit
+            }
+            result = insert_submission(db_conn, submission)
+            print "Insert result: ", result
+            for comment in sub.comments.list():
+                comm = {
+                    'id': comment.id,
+                    'body': comment.body,
+                    'sub_id': sub.id
+                }
+                result = insert_comment(db_conn, comm)
+                print "Comment insert result: ", result
     except Exception as e:
         print e.message
+
+def insert_submission(db, submission):
+    if db.Submissions.find_one({'id': submission['id']}):
+        print "submission already exists"
+        return submission['id']
+    else:
+        result = db.Submissions.insert_one(submission)
+        return result
+
+def insert_comment(db, comment):
+    if db.Comments.find_one({'id': comment['id']}):
+        print "comment already exists"
+        return comment['id']
+    else:
+        result = db.Comments.insert_one(comment)
+        return result
 
 def db_connect():
     try:
         client = MongoClient()
-        db = client.test
+        db = client.reddit
+        return db
     except Exception as e:
         print e.message
 
+def set_interval(func, sec):
+    def function_wrapper():
+        set_interval(func, sec)
+        func()
+    t = threading.Timer(sec, function_wrapper)
+    t.start()
+    return t
+
+def cleanDB(db):
+    db.Comments.remove({})
+    db.Submissions.remove({})
 
 
-load_credentials()
-make_call()
-# db_connect()
-# make_call()
+def main():
+    reddit = load_credentials()
+    db_conn = db_connect()
+    cleanDB(db_conn)
+    thread = set_interval(make_call(reddit, db_conn, "python"), 10)
+    # thread2 = set_interval(make_call(reddit, db_conn, "python"), 15)
+
+    # make_call(reddit, db_conn, "python")
+
+
+if __name__ == "__main__": main()
