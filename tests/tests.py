00f2f37ea74d025eb6222d8d9c54927e8e25b6c9
Stefan Ilie <ilie.stefan92@yahoo.com>
2017-05-26 16:52:43 +0300

    add some coverage to the tests

--- tests/tests.py
+++ tests/tests.py
@@ -2,7 +2,7 @@
 import os
 import sys
 import unittest
-from mock import MagicMock
+import mock
 
 MYDIR = os.path.dirname(__file__)
 sys.path.append(os.path.join(MYDIR, "../scrapper/"))
@@ -12,9 +12,33 @@ class ScrapperTestCase(unittest.TestCase):
     def setUp(self):
         unittest.TestCase.setUp(self)
 
-    def test_make_call(self):
+    def test_make_call_non_ascii(self):
         scrapperObj = scrapper.Scrapper()
         result = scrapperObj.make_call("¤©«±¶µ", 1495490048)
+        self.assertEqual(result, False)
+
+    def test_make_call(self):
+        test_json = {
+            "id": "1234123",
+            "title": "This is a test title",
+            "selftext": "Test is a test for testing tests.",
+            "num_comments": 45,
+            "created": 1495490048,
+            "subreddit": "test",
+            "comments": [
+                "this is the first comment",
+                "this is the second commment",
+                "this is the third comment"
+            ]
+        }
+        mock_reddit = mock.patch("scrapper.Scrapper.reddit.subreddit").start()
+        mock_insert = mock.patch("scrapper.Scrapper.insert_submission").start()
+        mock_insert_comment = mock.patch("scrapper.Scrapper.insert_comment").start()
+        mock_reddit.return_value = test_json
+        mock_insert.return_value = "6ejt4t"
+        mock_insert_comment.return_value = "3fas4gr"
+        scrapperObj = scrapper.Scrapper()
+        result = scrapperObj.make_call("python", 1495490048)
         self.assertEqual(result, False)
 
     def test_stage_one(self):
@@ -28,12 +52,12 @@ class ScrapperTestCase(unittest.TestCase):
 class FlaskTestCase(unittest.TestCase):
     def setUp(self):
         unittest.TestCase.setUp(self)
-
-    """
-    ToDo: Create tests for flask module
-    """
-    def test_flask_stage_one(self):
-        assertTrue(True)
+    #
+    # """
+    # ToDo: Create tests for flask module
+    # """
+    # def test_flask_stage_one(self):
+    #     assertTrue(True)
 
     def tearDown(self):
         unittest.TestCase.tearDown(self)
